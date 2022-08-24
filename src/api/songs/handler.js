ee822f2813c1315f4c9d9ac98733d3a5928d2cab
Muhammad Fakhryan <fakhryandev@gmail.com>
2022-08-24 22:47:11 +0700

    feat: delete song by id

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -9,6 +9,7 @@ class SongsHandler {
     this.getSongsHandler = this.getSongsHandler.bind(this)
     this.getSongByIdHandler = this.getSongByIdHandler.bind(this)
     this.putSongByIdHandler = this.putSongByIdHandler.bind(this)
+    this.deleteSongByIdHandler = this.deleteSongByIdHandler.bind(this)
   }
 
   async postSongHandler(request, h) {
@@ -117,6 +118,37 @@ class SongsHandler {
       return {
         status: 'success',
         message: 'Song berhasil diperbarui',
+      }
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        })
+        response.code(error.statusCode)
+
+        return response
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami',
+      })
+      response.code(500)
+      console.error(error)
+
+      return response
+    }
+  }
+
+  async deleteSongByIdHandler(request, h) {
+    try {
+      const { id } = request.params
+      await this._service.deleteSongById(id)
+
+      return {
+        status: 'success',
+        message: 'Song berhasil dihapus',
       }
     } catch (error) {
       if (error instanceof ClientError) {
